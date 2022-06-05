553b064e39ee573f7778f62b10cd679b68d6e885
MTTVDN <mattvdnieuwenhuijzen@gmail.com>
2022-06-05 17:14:59 +0200

    added backend partial sorter

---
+++ sorter-backend/partial_sorter.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"io"
+	"sort"
+	"strconv"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+type PubSubMessage struct {
+	Attributes map[string]string `json: "attributes"`
+	MessageId  string            `json: "messageId"`
+}
+
+func partialSort(ctx context.Context, m PubSubMessage) error {
+	// read pubsub
+	chunkIndex, err := strconv.Atoi(m.Attributes["index"])
+	chunkSize, err := strconv.Atoi(m.Attributes["chunksize"])
+	margin, err := strconv.Atoi(m.Attributes["margin"])
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	// read from cloud storage
+	partialString := make([]byte, chunkSize+margin)
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	bkt := client.Bucket("test")
+	obj := bkt.Object(m.Attributes["object"])
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+	n, err := r.ReadAt(partialString, chunkSize*chunkIndex)
+
+	// determine first and last newline of chunk
+	str := string(partialString)
+	firstNL := strings.Index(str, "\n")
+	lastNL := strings.Index(str[chunkSize:], "\n")
+	cut_str := str[firstNL:lastNL]
+
+	// split
+	split_str := strings.Fields(cut_str)
+
+	// sort
+	sort.Strings(split_str)
+
+	// store sorting result
+	newObjectName := m.Attributes["object"] + "-" + m.Attributes["index"]
+	resultObj := bkt.Object(newObjectName)
+	w := resultObj.NewWriter(ctx)
+	io.WriteString(w, split_str)
+
+	// determine if this is the last chunk
+	// if so, create pub/sub message for merging
+
+	// TODO
+
+	return nil
+}
