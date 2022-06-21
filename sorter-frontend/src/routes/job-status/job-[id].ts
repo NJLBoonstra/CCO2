03f8c5ad23012a17a9ed1e1bbbe9df3994661b2d
N.J.L. Boonstra <niels@nboonstra.nl>
2022-06-21 21:16:46 +0200

    Update job-status met nieuwe job interface

--- sorter-frontend/src/routes/job-status/job-[id].ts
+++ sorter-frontend/src/routes/job-status/job-[id].ts
@@ -1,15 +1,21 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { getJobStatus, type Job } from "$lib/googlecloud";
+import { getJobStatus, getPalindromeResult} from "$lib/googlecloud";
+import type { Job, PalindromeResult } from "$lib/job";
+
 
 /** @type {import('./__types/items').RequestHandler} */
 export async function get({params}) {
     const jobID: string = params.id;
-
     const jobStatus: Job = await getJobStatus(jobID);
+
+    let palindromeResult: PalindromeResult = await getPalindromeResult(jobID);
+
+
+    // let 
 
     return {
         body: {
-            jobStatus
+            jobStatus, palindromeResult
         }
     }
 }
