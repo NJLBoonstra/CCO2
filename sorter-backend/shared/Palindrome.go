11ec453b955108daa38887951b57e3934f696159
MTTVDN <mattvdnieuwenhuijzen@gmail.com>
2022-06-24 23:05:02 +0200

    update palindrome finder and palindrome firebase functions

--- sorter-backend/shared/Palindrome.go
+++ sorter-backend/shared/Palindrome.go
@@ -2,38 +2,94 @@ package job
 
 import (
 	"context"
+	"errors"
 
 	"cloud.google.com/go/firestore"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const CollectionPalindrome string = "palindromeResults"
 
 type PalindromeResult struct {
-	JobID             string `json:"jobId"`
-	Palindromes       int    `json:"palindromes"`
-	LongestPalindrome int    `json:"longestPalindrome"`
+	Palindromes       int `json:"palindromes"`
+	LongestPalindrome int `json:"longestPalindrome"`
 }
 
-func AddPalindromeResult(jobID string, palindromes int, longest int, fbClient *firestore.Client, ctx context.Context) error {
-	res := PalindromeResult{
-		JobID:             jobID,
-		Palindromes:       palindromes,
-		LongestPalindrome: longest,
-	}
+type PalindromeJob struct {
+	ID                     string                      `json:"id"`
+	PalindromeWorkerResult map[string]PalindromeResult `json:"palindromeWorkerResult"`
+	Palindromes            int                         `json:"palindromes"`
+	LongestPalindrome      int                         `json:"longestPalindrome"`
+}
 
+func CreatePalindromeTable(jobID string, fbClient *firestore.Client, ctx context.Context) error {
+
+	res := PalindromeJob{
+		ID:                     jobID,
+		PalindromeWorkerResult: map[string]PalindromeResult{},
+		Palindromes:            0,
+		LongestPalindrome:      0,
+	}
 	_, err := fbClient.Collection(CollectionPalindrome).Doc(jobID).Set(ctx, &res)
 
 	return err
 }
 
-func GetPalindromeResult(jobID string, fbClient *firestore.Client, ctx context.Context) (PalindromeResult, error) {
-	res := PalindromeResult{}
+func UpdatePalindromeResult(jobID string, workerUUID uuid.UUID, palindromes int, longest int, fbClient *firestore.Client, ctx context.Context) error {
+	docRef := fbClient.Collection(CollectionPalindrome).Doc(jobID)
+	j, err := docRef.Get(ctx)
+	if err != nil && status.Code(err) == codes.NotFound {
+		return errors.New("cannot update a non-existing PalindromeJob")
+	}
+
+	// TODO: dit misschien efficienter?
+	job := PalindromeJob{}
+	err = j.DataTo(&job)
+	if err != nil {
+		return err
+	}
+
+	// UPdate the document yeah
+	_, err = fbClient.Collection(CollectionPalindrome).Doc(jobID).Update(ctx, []firestore.Update{
+		{Path: "PalindromeWorkerResult." + workerUUID.String(), Value: PalindromeResult{Palindromes: palindromes, LongestPalindrome: longest}},
+	})
+	return err
+}
+
+func UpdatePalindromeJobResult(jobID string, palindromes int, longest int, fbClient *firestore.Client, ctx context.Context) error {
+	docRef := fbClient.Collection(CollectionPalindrome).Doc(jobID)
+	j, err := docRef.Get(ctx)
+	if err != nil && status.Code(err) == codes.NotFound {
+		return errors.New("cannot update a non-existing PalindromeJob")
+	}
+
+	// TODO: dit misschien efficienter?
+	job := PalindromeJob{}
+	err = j.DataTo(&job)
+	if err != nil {
+		return err
+	}
+
+	// UPdate the document yeah
+	_, err = fbClient.Collection(CollectionPalindrome).Doc(jobID).Update(ctx, []firestore.Update{
+		{Path: "Palindromes", Value: palindromes},
+	})
+	_, err = fbClient.Collection(CollectionPalindrome).Doc(jobID).Update(ctx, []firestore.Update{
+		{Path: "LongestPalindrome", Value: longest},
+	})
+	return err
+}
+
+func GetPalindromeResult(jobID string, fbClient *firestore.Client, ctx context.Context) (map[string]PalindromeResult, error) {
+	res := PalindromeJob{}
 	data, err := fbClient.Collection(CollectionPalindrome).Doc(jobID).Get(ctx)
 	if err != nil {
-		return res, err
+		return res.PalindromeWorkerResult, err
 	}
 
 	err = data.DataTo(&res)
 
-	return res, err
+	return res.PalindromeWorkerResult, err
 }
